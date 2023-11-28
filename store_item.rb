b4a12fb5897e210466d7b35f55df015381b1993c
ebanchik <ebanchik@gmail.com>
2023-11-28 13:36:25 -0500

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,8 @@
+#C1
+inventory = [{ item: "computer", color: "silver", release_year: 2023}, {item: "keyboard", type: "accessory", price: 99}, {item: "charger", power: "25V", price: 49}]
+
+#C2
+inventory = [{ item: "computer", color: "silver", price: 1999}, {item: "keyboard", color: "black", price: 99}, {item: "charger", color: "white", price: 49}]
+
+#C3
+inventory = [{ item: "computer", color: "silver", price: 1999}, {item: "keyboard", color: "black", price: 99}, {item: "charger", color: "white", price: 49}]
