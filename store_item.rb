d6677c8d4e0117dff469b70b940c05f2ad53f6db
ebanchik <ebanchik@gmail.com>
2023-11-28 15:07:01 -0500

    inputting array directly into method

--- store_item.rb
+++ store_item.rb
@@ -8,16 +8,33 @@
 # inventory = [{ item: "computer", color: "silver", price: 1999}, {item: "keyboard", color: "black", price: 99}, {item: "charger", color: "white", price: 49}]
 
 
+# class Items
+#   attr_accessor :item, :color, :price
+#   def initialize(item, color, price)
+#     @item = item
+#     @color = color
+#     @price = price
+#   end
+# end
+
+# inventory = Items.new("computer", "silver", 1999)
+# p inventory.item
+# p inventory.color
+# p inventory.price
+# inventory.price = 2200
+# p inventory.price
+
+
 class Items
   attr_accessor :item, :color, :price
-  def initialize(item, color, price)
-    @item = item
-    @color = color
-    @price = price
+  def initialize(input_items)
+    @item = input_items[:item]
+    @color = input_items[:color]
+    @price = input_items[:price]
   end
 end
 
-inventory = Items.new("computer", "silver", 1999)
+inventory = Items.new(item: "computer", color: "silver", price: 1999)
 p inventory.item
 p inventory.color
 p inventory.price
